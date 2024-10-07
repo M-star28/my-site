38c537df1bf5d3860dc8d6627e209a7d7f763e05
Your Namecls <you@example.com>
2024-10-07 19:49:26 +0200

    Initial Commit

---
+++ my_site/blog/models.py
@@ -0,0 +1,40 @@
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+# Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    excerpt = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='images')
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, null=True, on_delete=models.CASCADE, related_name='comments')
